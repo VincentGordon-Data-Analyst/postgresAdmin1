1d7fbe7099c41ae0af951d85fcc8d53bbe648a83
vincentgordon97 <66184004+vincentgordon97@users.noreply.github.com>
2023-04-23 15:31:55 -0400

    More quries

--- practiceSQL1.sql
+++ practiceSQL1.sql
@@ -12,5 +12,13 @@ CREATE TABLE house_price (
 	number_of_baths INT, 
 	extra_space BOOLEAN,
 	fire_place BOOLEAN
-	
 );
+
+-- Find houses in toronto that costs more that half a million
+SELECT * FROM house_price
+WHERE city = 'Toronto' 
+AND price > 500000;
+
+-- Order cities by price
+SELECT * FROM house_price
+ORDER BY price;
